062de7476541b5af48cadc72412983942bfd3963
Aileen Rowan <arowan@wesleyan.edu>
2013-06-11 15:14:10 -0400

    removed gupbos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130607205326) do
+ActiveRecord::Schema.define(:version => 20130611163000) do
+
+  create_table "gupbos", :force => true do |t|
+    t.string   "comment"
+    t.integer  "rating"
+    t.integer  "user_id"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "title"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.string   "image_remote_url"
+  end
 
   create_table "pins", :force => true do |t|
     t.string   "description"
--- test/unit/gupbo_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GupboTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
